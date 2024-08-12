b37366408d045218d3d839d02dc9e8175d586fbc
bigboss1331 <asc5188@gmail.com>
2024-08-12 16:45:13 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import { getContext } from "../../extensions.js";
-import {comfy_workflow} from "../stable-diffusion/index.js"
+//import {comfy_workflow} from "../stable-diffusion/index.js"
+import { generateQuietPrompt } from "../../../script.js";
+
 
 const context = getContext();
 context.chat; // Chat log
 context.characters; // Character list
 context.groups; // Group list
 // And many more...
-alert(comfy_workflow);
+
+function handleMessage(data) {
+    const text = data.message;
+    const translated = await generateQuietPrompt(text);
+    // ...
+}
